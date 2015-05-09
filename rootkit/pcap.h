8326051ffd1fa0ea819c8130d20aba618c0af711
x0r1 <gw22067@hotmail.com>
2015-05-09 13:04:49 +0930

    added tcp struct for seq,ack

--- rootkit/pcap.h
+++ rootkit/pcap.h
@@ -27,6 +27,31 @@ struct sniff_ip{
 #define IP_HL(ip)  (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip)   (((ip)->ip_vhl) >> 4)
 
+typedef unsigned int tcp_seq;
+
+// tcp header
+struct sniff_tcp{
+    unsigned short th_sport;  // source port
+    unsigned short th_dport;  // destination port
+    tcp_seq th_seq;	      // sequence number
+    tcp_seq th_ack;	      // acknowledgement number
+    unsigned char th_offx2;   // data offset, rsvd
+    #define TH_OFF(th)  (((th)->th_offx2 & 0xf0) >> 4)
+    unsigned char th_flags;
+    #define TH_FIN  0x01
+    #define TH_SYN  0x02
+    #define TH_RST  0x04
+    #define TH_PUSH 0x08
+    #define TH_ACK  0x10
+    #define TH_URG  0x20
+    #define TH_ECE  0x40
+    #define TH_CWR  0x80
+    #define TH_FLAGS  (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    unsigned short th_win;  // window
+    unsigned short th_sum;  // checksum
+    unsigned short th_urp;  // urgent pointer
+};
+
 // udp header
 struct sniff_udp{
     unsigned short udph_sport;
